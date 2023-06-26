56af82310a7494bf81f5e0143e1290d33dcd8fc5
kfujita <kfujita@student.42tokyo.jp>
2023-06-26 23:18:32 +0900

    complete ex02

---
+++ ex02/ClapTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/24 19:33:02 by kfujita           #+#    #+#             */
+/*   Updated: 2023/06/26 22:57:47 by kfujita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+
+class ClapTrap
+{
+protected:
+	std::string name;
+	unsigned int points;
+	unsigned int energyPoints;
+	unsigned int attackDamage;
+
+public:
+	ClapTrap();
+	ClapTrap(const std::string& name);
+	ClapTrap(const ClapTrap& from);
+	~ClapTrap();
+
+	ClapTrap& operator=(const ClapTrap& from);
+
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
