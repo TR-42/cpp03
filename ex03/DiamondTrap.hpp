4881fc5461519e2fe05520a39212bb9e129b4b82
kfujita <kfujita@student.42tokyo.jp>
2023-08-26 13:22:23 +0900

    ダイヤモンド継承を明示した
    本当は中間クラスのvirtual継承だけで大丈夫なはず。

--- ex03/DiamondTrap.hpp
+++ ex03/DiamondTrap.hpp
@@ -15,7 +15,7 @@
 #include "FragTrap.hpp"
 #include "ScavTrap.hpp"
 
-class DiamondTrap : public FragTrap, public ScavTrap
+class DiamondTrap : public virtual FragTrap, public virtual ScavTrap
 {
 private:
 	std::string name;
